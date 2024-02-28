8500180dea847da10cc39daeb1b2bcc4d5d1752c
khavyakj <160817279+khavyakj@users.noreply.github.com>
2024-02-28 09:55:35 +0530

    Create dflipflop.tb.v

---
+++ VERILOG/day3/dflipflop.tb.v
@@ -0,0 +1,33 @@
+// Code your testbench here
+// or browse Examples
+module tb;
+reg D;
+reg clk;
+reg reset;
+wire Q;
+
+d_flip_flop d1(Q,D,clk,reset);
+  initial begin
+    $dumpfile("d_flip_flop.vcd");
+    $dumpvars(0,d_flip_flop_tb);
+  end
+
+initial begin 
+    clk = 1'b0;
+    forever #20 clk = ~clk ;
+  end
+  
+initial 
+  begin
+    reset = 1'b1;
+    #40;
+    reset = 1'b0;
+    #40;
+    D = 1'b0;
+    #40;
+    D = 1'b1;
+    #40;
+    
+    $finish ;
+  end
+endmodule   
