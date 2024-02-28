d84b54cad759bbf8662253abaa481be43849d4ed
khavyakj <160817279+khavyakj@users.noreply.github.com>
2024-02-28 10:08:45 +0530

    Update dlatch.tb.v

--- VERILOG/day3/dlatch.tb.v
+++ VERILOG/day3/dlatch.tb.v
@@ -1,1 +1,33 @@
+// Code your testbench here
+// or browse Examples
+module testbench;
 
+    // Inputs
+    reg D;
+    reg EN;
+
+    // Outputs
+    wire Q;
+
+    // Instantiate the D latch
+    d_latch UUT (
+        .D(D),
+        .EN(EN),
+        .Q(Q)
+    );
+
+    // Stimulus
+    initial begin
+        // Initialize inputs
+        D = 1'b0;
+        EN = 1'b0;
+
+        // Change inputs
+        #10 D = 1'b1; EN = 1'b1;
+        #10 D = 1'b0; EN = 1'b1;
+        #10 D = 1'b1; EN = 1'b0;
+        #10 D = 1'b0; EN = 1'b0;
+
+        // End simulation
+        #10 $finish;
+    end
