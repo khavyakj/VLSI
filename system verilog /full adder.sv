0447f3e36bfdb53f36afa69fd069f7b30f7546e6
khavyakj <160817279+khavyakj@users.noreply.github.com>
2024-06-19 19:16:22 +0530

    Create full adder.sv

---
+++ system verilog /full adder.sv
@@ -0,0 +1,13 @@
+
+module half_addr(input a, b, output so, co);
+  assign so = a^b;
+  assign co = a & b;
+endmodule
+
+module fulladd(fa inf);
+  wire s0, c0, c1;
+  half_addr HA1 (inf.a, inf.b, s0, c0);
+  half_addr HA2 (s0, inf.c, inf.s_out, c1);
+  
+  assign inf.c_out = c0 | c1;
+endmodule
