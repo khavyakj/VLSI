0018c3dd41fd98ceb043182aac01f35233b4edb7
khavyakj <160817279+khavyakj@users.noreply.github.com>
2024-06-28 10:17:37 +0530

    Update datatype logic.sv

--- SYSTEM VERILOG/datatype logic.sv
+++ SYSTEM VERILOG/datatype logic.sv
@@ -1,1 +1,10 @@
-
+// Code your design here
+module logic_data;
+  logic [2:0]logic_datatype;
+  initial begin
+    $display("\ndefault value of logic data typ=%b",logic_datatype);
+    logic_datatype=3'b101;
+    $display("\n value of logic datatype after initialization =%b",logic_datatype);
+    $display("");
+  end
+endmodule: logic_data
